7e62c2d786a2480b36ab5ce4cb8c5c4fa01ebbae
Dovydas <dovius.vili@gmail.com>
2017-03-08 18:49:00 +0200

    java init

---
+++ second/src/CPU.java
@@ -0,0 +1,19 @@
+/**
+ * Created by dovydas on 17.3.8.
+ */
+public class CPU {
+  public byte[] mode = {0};
+  public byte[] ptr = {0, 0};
+  public byte[] sp = {0, 0, 0};
+  public byte[] pc = {0, 0, 0};
+  public byte[] r = {0, 0, 0};
+  public byte[] ti = {0};
+  public byte[] si = {0};
+  public byte[] pi = {0};
+  public byte[] ioi = {0};
+  public byte[] ch1 = {0};
+  public byte[] ch2 = {0};
+  public byte[] ch3 = {0};
+  public byte[] sd = {0};
+
+}
---
+++ second/src/PhysicalMachine.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dovydas on 17.3.8.
+ */
+public class PhysicalMachine {
+  private CPU cpu;
+
+  public PhysicalMachine() {
+    cpu = new CPU();
+  }
+}
